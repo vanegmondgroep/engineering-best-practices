a400560d1d383abe9d02536284e99cea89a5f558
Nick Jansen <n.jansen@sitepilot.io>
2021-08-26 14:36:49 +0200

    Implement vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    title: "Engineering Best Practices",
+    description: "This guide dictates how we, as a company, engineer applications.",
+    base: '/',
+
+    //head: require('./head'),
+
+    themeConfig: {
+        logo: '/assets/img/logo.png',
+        displayAllHeaders: true,
+        activeHeaderLinks: false,
+        searchPlaceholder: 'Press / to search',
+        lastUpdated: false, // string | boolean
+        sidebarDepth: 1,
+
+        repo: 'vanegmondgroep/engineering-best-practices',
+
+        docsRepo: 'vanegmondgroep/engineering-best-practices',
+        editLinks: true,
+        editLinkText: 'Help us improve this page!',
+        docsBranch: 'main',
+
+        nav: [
+            { text: 'Home', link: '/', target: '_self' },
+        ],
+
+        sidebar: [
+            '/',
+            '/Docker',
+            '/Git'
+        ]
+    },
+}
---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+These are the official best practices for Van Egmond Groep. This guide dictates how we, as a company, engineer applications. The purpose behind them is to improve the quality of the experiences we build as well as to standardize in order to facilitate more effective collaboration.
